c8133f6452d6ff77ddb682b710bcd286445db8ec
Jeffongoro <ongorogc@gmail.com>
2023-07-27 22:51:14 +0300

    setup react tailwind and vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" bg-slate-400 w-full overflow-hidden">
+      <h1 className="text-3xl font-bold  text-center">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
